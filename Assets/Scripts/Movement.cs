a4806113405de3ea94a393b1b37ac0a9c41499c8
Shaunak Aghor <69709460+ChokingTrapper@users.noreply.github.com>
2025-03-19 19:31:50 +0530

    jump check methods

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,20 +7,27 @@ public class sample : MonoBehaviour
     public float movementSpeed = 5f;
 
     public float jumpVelocity = 5f;
+    public float maxdist = 1.25f;
+    public LayerMask groundlayer;
 
     Rigidbody2D rb;
     bool canJump;
+    bool isGrounded;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
     }
 
     void Update()
     {
+       // isGrounded = Physics2D.Raycast(transform.position, Vector2.down,maxdist, groundlayer);
+        //Debug.DrawRay(transform.position, Vector2.down * maxdist, Color.green );
+        
+        Debug.Log(isGrounded);
         float inputHorizontal = Input.GetAxisRaw("Horizontal");
         bool canJump1 = Input.GetKeyDown(KeyCode.Space);
-        if(canJump1) canJump = true;
 
         // float inputVertical = Input.GetAxisRaw("Vertical");
         // transform.Rotate(0f,0f,rotationSpeed * -1 * inputHorizontal * Time.deltaTime);
@@ -31,15 +38,21 @@ public class sample : MonoBehaviour
 
         rb.linearVelocity = new Vector2(inputHorizontal * movementSpeed, rb.linearVelocity.y);
 
-        
-    }
-
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        if(canJump)
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpVelocity);
-            canJump = false;
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            Jump();
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        isGrounded = true;
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpVelocity);
+        isGrounded = false;
+    }
+
+
 }
